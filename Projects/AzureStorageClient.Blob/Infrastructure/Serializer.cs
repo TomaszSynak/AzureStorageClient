0e4b3842f4873f98c8080f00e64ba5e5d532e927
Tomasz Synak <tomasz.synak@outlook.com>
2020-11-13 02:04:42 +0100

    [patch] (BlobStorageClient) JSON serializer should not lowercase property name

--- Projects/AzureStorageClient.Blob/Infrastructure/Serializer.cs
+++ Projects/AzureStorageClient.Blob/Infrastructure/Serializer.cs
@@ -5,9 +5,15 @@
 
     internal static class Serializer
     {
+        /// <summary>
+        /// You should not override the Newtonsoft.Json ContractResolver with CamelCasePropertyNamesContractResolver for Json Serialization.
+        /// Newtonsoft.Json by default respects the casing used in property/field names which is typically PascalCase.
+        /// This can be overriden to serialize the names to camelCase and blob client will store the JSON in the storage as specified by the Newtonsoft.Json settings.
+        /// https://martendb.io/documentation/documents/json/newtonsoft/
+        /// </summary>
         private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
         {
-            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            ContractResolver = new DefaultContractResolver(),
             ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor,
             Formatting = Formatting.Indented,
             DateFormatString = "o",
@@ -15,13 +21,13 @@
         };
 
         public static string Serialize<T>(this T objectToSerialize)
-            where T : class, new()
+            where T : class
         {
             return JsonConvert.SerializeObject(objectToSerialize, JsonSerializerSettings);
         }
 
         public static T Deserialize<T>(this string contentToDeserialize)
-            where T : class, new()
+            where T : class
         {
             return JsonConvert.DeserializeObject<T>(contentToDeserialize, JsonSerializerSettings);
         }
---
+++ Tests/AzureStorageClient.IntegrationTests/AzureBlobClient/SerializerTests.cs
@@ -0,0 +1,30 @@
+﻿namespace AzureStorageClient.IntegrationTests.AzureBlobClient
+{
+    using System;
+    using System.Linq;
+    using Xunit;
+
+    [Collection(nameof(IntegrationTests))]
+    public class SerializerTests
+    {
+        /// <summary>
+        /// You should not override the Newtonsoft.Json ContractResolver with CamelCasePropertyNamesContractResolver for Json Serialization.
+        /// Newtonsoft.Json by default respects the casing used in property/field names which is typically PascalCase.
+        /// This can be overriden to serialize the names to camelCase and blob client will store the JSON in the storage as specified by the Newtonsoft.Json settings.
+        /// https://martendb.io/documentation/documents/json/newtonsoft/
+        /// </summary>
+        [Fact]
+        public void SerializerSettings_PropertyNameShouldNotBeLowercased()
+        {
+            // Arrange
+            var (testModel, _, _) = TestModelFactory.Create();
+
+            // Act
+            var testModelSerialized = testModel.Serialize();
+            var deserializedTestModel = testModelSerialized.Deserialize<TestModel>();
+
+            // Assert
+            Assert.Equal(testModel.PrivateData.Keys.First(), deserializedTestModel.PrivateData.Keys.First(), StringComparer.InvariantCulture);
+        }
+    }
+}
